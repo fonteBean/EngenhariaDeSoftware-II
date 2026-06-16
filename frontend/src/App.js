1608109ed8e2619a36bb177c9878ed8234766b8e
Nicolas Bean <n9ckbean@gmail.com>
2026-06-16 18:29:33 -0300

    Fix: Rotas alteradas && Remove: Front antigo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ContatoList from './components/ContatoList';
-import ContatoForm from './components/ContatoForm';
-import CompromissoList from './components/CompromissoList';
-import CompromissoForm from './components/CompromissoForm';
+
+import ProfissionalList from './components/ProfissionalList';
+import ProfissionalForm from './components/ProfissionalForm';
+
+import AtendimentoList from './components/AtendimentoList';
+import AtendimentoForm from './components/AtendimentoForm';
+
+import ExameList from './components/ExameList';
+import ExameForm from './components/ExameForm';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar">
-          <h1>📅 Agenda Web</h1>
-          <div className="nav-links">
-            <Link to="/contatos">Contatos</Link>
-            <Link to="/compromissos">Compromissos</Link>
-          </div>
-        </nav>
+        <header>
+          <h1>Sistema de Saúde</h1>
 
-        <main className="container">
+          <nav>
+            <Link to="/profissionais">Profissionais</Link>
+            {' | '}
+            <Link to="/atendimentos">Atendimentos</Link>
+            {' | '}
+            <Link to="/exames">Exames</Link>
+          </nav>
+        </header>
+
+        <main>
           <Routes>
-            <Route path="/" element={<ContatoList />} />
-            <Route path="/contatos" element={<ContatoList />} />
-            <Route path="/contatos/novo" element={<ContatoForm />} />
-            <Route path="/contatos/editar/:id" element={<ContatoForm />} />
-            <Route path="/compromissos" element={<CompromissoList />} />
-            <Route path="/compromissos/novo" element={<CompromissoForm />} />
-            <Route path="/compromissos/editar/:id" element={<CompromissoForm />} />
+            <Route path="/" element={<ProfissionalList />} />
+
+            <Route path="/profissionais" element={<ProfissionalList />} />
+            <Route path="/profissionais/novo" element={<ProfissionalForm />} />
+            <Route path="/profissionais/editar/:id" element={<ProfissionalForm />} />
+
+            <Route path="/atendimentos" element={<AtendimentoList />} />
+            <Route path="/atendimentos/novo" element={<AtendimentoForm />} />
+            <Route path="/atendimentos/editar/:id" element={<AtendimentoForm />} />
+
+            <Route path="/exames" element={<ExameList />} />
+            <Route path="/exames/novo" element={<ExameForm />} />
+            <Route path="/exames/editar/:id" element={<ExameForm />} />
           </Routes>
         </main>
       </div>
